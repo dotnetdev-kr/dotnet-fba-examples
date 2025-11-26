92583fd9293b01138d53496e82f8a6fd7eed518b
Jung Hyun Nam <rkttu@rkttu.com>
2025-11-26 22:36:17 +0900

    Add file and directory path logging

--- 01-shell-standard.cs
+++ 01-shell-standard.cs
@@ -1,2 +1,7 @@
 #!/usr/bin/env dotnet
 Console.WriteLine("No lemon, no melon.");
+
+var filePath = AppContext.GetData("EntryPointFilePath") as string;
+var directoryPath = AppContext.GetData("EntryPointFileDirectoryPath") as string;
+Console.WriteLine($"File: {filePath}");
+Console.WriteLine($"Directory: {directoryPath}");
