bac1adae62edb159060fbbfad4a8b7a03f11eff5
Jung Hyun Nam <rkttu@rkttu.com>
2025-11-26 22:36:28 +0900

    Add file and directory path logging

--- 01-shell-alt.cs
+++ 01-shell-alt.cs
@@ -1,2 +1,7 @@
 ///usr/bin/env dotnet "$0" "$@" ; exit $?
 Console.WriteLine("No lemon, No melon.");
+
+var filePath = AppContext.GetData("EntryPointFilePath") as string;
+var directoryPath = AppContext.GetData("EntryPointFileDirectoryPath") as string;
+Console.WriteLine($"File: {filePath}");
+Console.WriteLine($"Directory: {directoryPath}");
